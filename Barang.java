b69a0bf3b791fc3e21d1d9b4c5b50299e3876e57
hannypm <101863648+hannypm@users.noreply.github.com>
2022-12-10 23:21:43 +0700

    Add files via upload

---
+++ Barang.java
@@ -0,0 +1,12 @@
+package PBO;
+
+public abstract class Barang {
+    protected int nomer;
+    protected String barang;
+    protected int stokBarang;
+    protected int harga;
+
+    abstract void display();
+
+    abstract void pilihBarang();
+}
---
+++ User.java
@@ -0,0 +1,23 @@
+package PBO;
+
+import java.util.Scanner;
+public class User {
+    private int uang;
+    private int noBarang;
+
+    public int getUang() {
+        return uang;
+    }
+
+    public void setUang(int uang) {
+        this.uang = uang;
+    }
+
+    public int getNoBarang() {
+        return noBarang;
+    }
+
+    public void setNoBarang(int noBarang) {
+        this.noBarang = noBarang;
+    }
+}
